371d42a30d43d01bb2cb1e0179dec8b1e6b9eca2
swift_file <sky.y.m.318@gmail.com>
2022-11-15 18:25:41 +0900

    change dust_result_pub to single and multi_result_pub

--- sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/blockage_diag/blockage_diag_nodelet.hpp
+++ sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/blockage_diag/blockage_diag_nodelet.hpp
@@ -50,7 +50,8 @@ protected:
   rcl_interfaces::msg::SetParametersResult paramCallback(const std::vector<rclcpp::Parameter> & p);
   image_transport::Publisher lidar_depth_map_pub_;
   image_transport::Publisher blockage_mask_pub_;
-  image_transport::Publisher dust_result_img_pub;
+  image_transport::Publisher single_frame_dust_mask_pub;
+  image_transport::Publisher multi_frame_dust_mask_pub;
   image_transport::Publisher blockage_dust_merged_pub;
   rclcpp::Publisher<tier4_debug_msgs::msg::Float32Stamped>::SharedPtr ground_blockage_ratio_pub_;
   rclcpp::Publisher<tier4_debug_msgs::msg::Float32Stamped>::SharedPtr sky_blockage_ratio_pub_;
@@ -69,7 +70,8 @@ private:
   float sky_blockage_ratio_ = -1.0f;
   std::vector<float> ground_blockage_range_deg_ = {0.0f, 0.0f};
   std::vector<float> sky_blockage_range_deg_ = {0.0f, 0.0f};
-  int erode_kernel_ = 10;
+  int blockage_kernel_ = 10;
+  int blockage_frame_count_ = 0;
   uint ground_blockage_count_ = 0;
   uint sky_blockage_count_ = 0;
   uint blockage_count_threshold_;
